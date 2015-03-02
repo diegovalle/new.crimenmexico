8ec85bc19dcc79066c41d83a5935a2b2af4d0684
Diego Valle-Jones <diegovalle@gmail.com>
2015-03-01 21:30:41 -0600

    download snsp data

---
+++ downloader/lib/clean_states.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python # -*- coding: utf-8 -*-
+import sqlite3 as sq
+import pandas as pd
+import numpy as np
+import os
+import pandas.io.sql as pd_sql
+import math
+
+
+class CrimeStates:
+    """"""
+    _DATADIR = 'data'
+    _columnNames = ['year', 'state',
+                    'modalidad', 'tipo', 'subtipo',
+                    '01', '02', '03', '04', '05', '06',
+                    '07', '08', '09', '10', '11', '12']
+    _cleanColumnNames = ['modalidad', 'tipo', 'subtipo', 'count', 'state_code', 'date']
+    _columnOrder = ['state_code',
+                    'modalidad', 'tipo', 'subtipo', 'date', 'count']
+    _rawColumns = [u'AÑO', u'ENTIDAD',
+                   u'MODALIDAD', u'TIPO', u'SUBTIPO',
+                   u'ENERO', u'FEBRERO',
+                   u'MARZO', u'ABRIL', u'MAYO', u'JUNIO',
+                   u'JULIO', u'AGOSTO', u'SEPTIEMBRE',
+                   u'OCTUBRE', u'NOVIEMBRE', u'DICIEMBRE']
+    _ENTIDAD = [u'AGUASCALIENTES', u'BAJA CALIFORNIA', u'BAJA CALIFORNIA SUR',
+                u'CAMPECHE', u'COAHUILA', u'COLIMA', u'CHIAPAS', u'CHIHUAHUA',
+                u'DISTRITO FEDERAL', u'DURANGO', u'GUANAJUATO', u'GUERRERO',
+                u'HIDALGO', u'JALISCO', u'MEXICO', u'MICHOACAN', u'MORELOS',
+                u'NAYARIT', u'NUEVO LEON', u'OAXACA', u'PUEBLA', u'QUERETARO',
+                u'QUINTANA ROO', u'SAN LUIS POTOSI', u'SINALOA', u'SONORA',
+                u'TABASCO', u'TAMAULIPAS', u'TLAXCALA', u'VERACRUZ', u'YUCATAN',
+                u'ZACATECAS']
+    _MODALIDAD = [u'DELITOS PATRIMONIALES', u'DELITOS SEXUALES (VIOLACION)',
+                  u'HOMICIDIOS', u'LESIONES', u'OTROS DELITOS',
+                  u'PRIV. DE LA LIBERTAD (SECUESTRO)', u'ROBO COMUN',
+                  u'ROBO DE GANADO (ABIGEATO)', u'ROBO EN INSTITUCIONES BANCARIAS',
+                  u'ROBO EN CARRETERAS']
+    _TIPO = [u'ABUSO DE CONFIANZA', u'DA\xd1O EN PROPIEDAD AJENA', u'EXTORSION',
+             u'FRAUDE', u'DESPOJO', u'VIOLACION', u'DOLOSOS', u'CULPOSOS',
+             u'CULPOSAS', u'AMENAZAS', u'ESTUPRO', u'OTROS SEXUALES',
+             u'RESTO DE LOS DELITOS (OTROS)', u'SECUESTRO', u'CON VIOLENCIA',
+             u'SIN VIOLENCIA', u'ABIGEATO', u'DOLOSAS']
+    _SUBTIPO = [u'ABUSO DE CONFIANZA', u'DA\xd1O EN PROPIEDAD AJENA', u'EXTORSION',
+                u'FRAUDE', u'CON VIOLENCIA', u'SIN VIOLENCIA', u'SIN DATOS',
+                u'VIOLACION', u'CON ARMA DE FUEGO', u'CON ARMA BLANCA', u'OTROS',
+                u'AMENAZAS', u'ESTUPRO', u'OTROS SEXUALES',
+                u'RESTO DE LOS DELITOS (OTROS)', u'SECUESTRO', u'A CASA HABITACION',
+                u'A NEGOCIO', u'DE VEHICULOS', u'A TRANSPORTISTAS',
+                u'A TRANSEUNTES', u'ABIGEATO', u'A BANCOS', u'A CASA DE BOLSA',
+                u'A CASA DE CAMBIO', u'A EMPRESA DE TRASLADO DE VALORES',
+                u'A CAMIONES DE CARGA', u'A AUTOBUSES', u'A VEHICULOS PARTICULARES']
+
+    data = pd.DataFrame()
+    modalidad = pd.DataFrame()
+    tipo = pd.DataFrame()
+    subtipo = pd.DataFrame()
+    population = pd.DataFrame()
+    state_codes = pd.DataFrame()
+
+    def __init__(self, fname):
+        self.clean_file(fname)
+
+    def check_column(self, df, col, values):
+        assert (np.all(sorted(df[col].unique()) == sorted(values)))
+
+    def get_uniq_values(self, df, col):
+        values = df[col].unique()
+        values_codes = dict(zip(values, range(0, len(values))))
+        return values_codes
+
+    def get_uniq_df(self, df, col):
+        values_codes = self.get_uniq_values(df, col)
+        values_df = pd.DataFrame(values_codes.items(), columns=[col + '_text', col])
+        return values_df.sort(col)
+
+    def check_file(self, df):
+        assert (np.all(df.columns == self._rawColumns))
+        self.check_column(df, 'ENTIDAD', self._ENTIDAD)
+        self.check_column(df, 'MODALIDAD', self._MODALIDAD)
+        self.check_column(df, 'TIPO', self._TIPO)
+        self.check_column(df, 'SUBTIPO', self._SUBTIPO)
+
+    def clean_file(self, fname):
+        df = pd.read_csv(fname,  # 'snsp-data/IncidenciaDelictiva_FueroComun_Estatal_1997-2015.csv'
+                         encoding="windows-1252", thousands=",")
+
+        self.state_codes = pd.read_csv(os.path.join(self._DATADIR, "state_codes.csv"))
+        population = pd.read_csv(os.path.join(self._DATADIR, "pop_states.csv"))
+        population['date'] = population.date.str.slice(0, 7)
+        self.population = population
+
+        self.check_file(df)
+
+        self.modalidad = self.get_uniq_df(df, 'MODALIDAD')
+        self.tipo = self.get_uniq_df(df, 'TIPO')
+        self.subtipo = self.get_uniq_df(df, 'SUBTIPO')
+
+        df = df.replace({'MODALIDAD': self.get_uniq_values(df, 'MODALIDAD'),
+                         'TIPO': self.get_uniq_values(df, 'TIPO'),
+                         'SUBTIPO': self.get_uniq_values(df, 'SUBTIPO'),
+                         })
+
+        df.columns = self._columnNames
+
+        df = pd.melt(df, id_vars=['year', 'state', 'modalidad', 'tipo', 'subtipo'])
+        df = pd.merge(df, self.state_codes)
+        df['date'] = df["year"].map(str) + '-' + df['variable']
+        del df['variable']  # delete month
+        del df['year']
+        del df['state']
+
+        df.columns = self._cleanColumnNames
+
+        df = df[self._columnOrder]
+        df.count = df['count'].str.replace(',', '')
+
+        # The SNSP reports months in the future as NA so get rid of them
+        # bad_dates = []
+        for date in reversed(sorted(df.date.unique())):
+            if (all(df[df['date'] == date]['count'].convert_objects(convert_numeric=True).map(np.isnan)) |
+                    all(df[df['date'] == date]['count'] == 0)):
+                print(date)
+                # bad_dates.append(date)
+                df = df[df['date'] != date]
+            else:
+                break
+        # if len(bad_dates):
+            # df = df.query('date != ' + ' & date != '.join(bad_dates))
+
+
+
+        self.data = df
+
+
+class CrimeMunicipios(CrimeStates):
+    """
+    """
+    _rawColumns = [u'AÑO', u'INEGI', u'ENTIDAD', u'MUNICIPIO', u'MODALIDAD', u'TIPO',
+                   u'SUBTIPO', u'ENERO', u'FEBRERO', u'MARZO', u'ABRIL', u'MAYO', u'JUNIO',
+                   u'JULIO', u'AGOSTO', u'SEPTIEMBRE', u'OCTUBRE', u'NOVIEMBRE', u'DICIEMBRE']
+    _columnNames = ['year', 'inegi', 'state', 'municipio',
+                    'modalidad', 'tipo', 'subtipo',
+                    '01', '02', '03', '04', '05', '06',
+                    '07', '08', '09', '10', '11', '12']
+    _cleanColumnNames = ['modalidad', 'tipo', 'subtipo', 'count', 'state_code', 'mun_code', 'date']
+    _columnOrder = ['state_code', 'mun_code',
+                    'modalidad', 'tipo', 'subtipo', 'date', 'count']
+    municipios = pd.DataFrame()
+
+    def clean_file(self, fname):
+        df = pd.read_csv(fname,  # 'snsp-data/IncidenciaDelictiva_FueroComun_Estatal_1997-2015.csv'
+                         encoding="windows-1252", thousands=",",
+                         dtype={u'AÑO': int, u'INEGI': int, u'ENTIDAD': str, u'MUNICIPIO': str, u'MODALIDAD': str,
+                                u'TIPO': str,
+                                u'SUBTIPO': str, u'ENERO': str, u'FEBRERO': str, u'MARZO': str, u'ABRIL': str,
+                                u'MAYO': str, u'JUNIO': str,
+                                u'JULIO': str, u'AGOSTO': str, u'SEPTIEMBRE': str, u'OCTUBRE': str, u'NOVIEMBRE': str,
+                                u'DICIEMBRE': str})
+
+        # state_codes = pd.read_csv(os.path.join(self._DATADIR, "state_codes.csv"))
+        population = pd.read_csv(os.path.join(self._DATADIR, "pop_muns.csv"))
+        population['date'] = population.date.str.slice(0, 7)
+        self.population = population
+
+        # There's an error in the SNSP data
+        df['SUBTIPO'] = df['SUBTIPO'].replace(u'POR ARMA DE FUEGO', u'CON ARMA DE FUEGO')
+        df['SUBTIPO'] = df['SUBTIPO'].replace(u'POR ARMA BLANCA', u'CON ARMA BLANCA')
+        self.check_file(df)
+
+        self.modalidad = self.get_uniq_df(df, 'MODALIDAD')
+        self.tipo = self.get_uniq_df(df, 'TIPO')
+        self.subtipo = self.get_uniq_df(df, 'SUBTIPO')
+
+        df = df.replace({'MODALIDAD': self.get_uniq_values(df, 'MODALIDAD'),
+                         'TIPO': self.get_uniq_values(df, 'TIPO'),
+                         'SUBTIPO': self.get_uniq_values(df, 'SUBTIPO'),
+                         })
+
+        df.columns = self._columnNames
+
+        df = pd.melt(df, id_vars=['year', 'inegi', 'state', 'municipio', 'modalidad', 'tipo', 'subtipo'])
+        df['state_code'] = df['inegi'].apply(lambda x: math.floor(x / 1000)).astype(int)
+        df['mun_code'] = df['inegi'].apply(lambda x: x % 1000)
+
+        self.municipios = pd.concat([df['state_code'], df['mun_code'], df['municipio']], axis=1).drop_duplicates()
+
+        df['date'] = df["year"].map(str) + '-' + df['variable']
+        del df['variable']  # delete month
+        del df['year']
+        del df['state']
+        del df['inegi']
+        del df['municipio']
+
+        df.columns = self._cleanColumnNames
+
+        df = df[self._columnOrder]
+        df.count = df['count'].str.replace(',', '')
+
+        # The SNSP reports months in the future as NA so get rid of them
+        # bad_dates = []
+        for date in reversed(sorted(df.date.unique())):
+            if (all(df[df['date'] == date]['count'].convert_objects(convert_numeric=True).map(np.isnan)) |
+                    all(df[df['date'] == date]['count'] == 0)):
+                print(date)
+                # bad_dates.append(date)
+                df = df[df['date'] != date]
+            else:
+                break
+        # if len(bad_dates):
+            # df = df.query('date != ' + ' & date != '.join(bad_dates))
+
+        self.data = df
+
---
+++ downloader/lib/victimas_comun.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python # -*- coding: utf-8 -*-
+from __future__ import print_function
+from titlecase import titlecase
+import pandas as pd
+import glob
+import os
+import pdb
+import unicodedata
+import numpy as np
+
+def strip_accents(input_str):
+    nkfd_form = unicodedata.normalize('NFKD', input_str)
+    return u"".join([c for c in nkfd_form if not unicodedata.combining(c)])
+
+def toupper(s):
+    return s.decode( 'utf-8' ).upper()
+
+
+columnNames = ['state', 'month', 'count',
+               'year', 'date', 'modalidad', 'tipo', 'subtipo']
+columnOrder = ['state', 'state_code', 'modalidad', 'tipo', 'subtipo', 'date', 'year', 'month', 'count', 'population']
+mapping = [('Enero', '01'), ('Febrero', '02'),
+           ('Marzo', '03'), ('Abril', '04'),
+           ('Mayo', '05'), ('Junio', '06'),
+           ('Julio', '07'), ('Agosto', '08'),
+           ('Septiembre', '09'), ('Octubre', '10'),
+           ('Noviembre', '11'), ('Diciembre', '12')]
+months = ['Enero','Febrero',
+          'Marzo','Abril',
+          'Mayo', 'Junio',
+          'Julio', 'Agosto',
+          'Septiembre', 'Octubre',
+          'Noviembre','Diciembre']
+
+
+
+def clean_comun(file):
+    print(file)
+    victimas = pd.DataFrame()
+    state_codes = pd.read_csv("data/state_codes.csv")
+    population = pd.read_csv("data/pop_states.csv")
+    df = pd.read_csv("victimas-csv/" + file)
+
+    #assert structure of file
+    assert(all(df.columns == [u'Entidad federativa', u'Enero', u'Febrero', u'Marzo',
+                              u'Abril', u'Mayo', u'Junio', u'Julio', u'Agosto',
+                              u'Septiembre', u'Octubre', u'Noviembre', u'Diciembre',
+                              u'Total']))
+    # If the column is all 0s treat is as if it were data that hasn't happened
+    for month in reversed(months):
+        col = (df[month] == 0)
+        if col.count() == col.sum():
+            df = df.drop(month, 1)
+            print("Warning: Deleteing column -" + month + "- containg all 0")
+        else:
+            break
+
+    df["Entidad federativa"] = df["Entidad federativa"].map(toupper)
+    df["Entidad federativa"] = df["Entidad federativa"].map(strip_accents)
+
+    df = pd.melt(df, id_vars = "Entidad federativa")
+    df = df[df.variable != "Total"].copy()
+    #pdb.set_trace()
+    if "victima_2014" in file:
+        df["year"] = 2014
+    if "victima_2015" in file:
+        df["year"] = 2015
+    for k, v in mapping:
+        df['variable'] = df['variable'].replace(k, v)
+    df['date'] = df["year"].map(str) + '-' + df['variable'] + '-01'
+    if "homicidio_doloso" in file:
+        df["modalidad"] = "HOMICIDIOS"
+        df['tipo'] = "DOLOSOS"
+        df['subtipo'] = "TOTAL"
+    if "homicidio_culposo" in file:
+        df["modalidad"] = "HOMICIDIOS"
+        df['tipo'] = "CULPOSOS"
+        df['subtipo'] = "TOTAL"
+    if "secuestro" in file:
+        df["modalidad"] = "PRIV. DE LA LIBERTAD (SECUESTRO)"
+        df['tipo'] = "SECUESTRO"
+        df['subtipo'] = "SECUESTRO"
+    if "extorsion" in file:
+        df["modalidad"] = "DELITOS PATRIMONIALES"
+        df['tipo'] = "EXTORSION"
+        df['subtipo'] = "EXTORSION"
+    df.columns = columnNames
+    victimas = victimas.append(df)
+
+
+    victimas = pd.merge(victimas, state_codes, how = 'left')
+
+    victimas = pd.merge(victimas, population, how = 'left')
+
+    victimas = victimas[victimas['state'] != 'NACIONAL']
+    victimas = victimas.sort(['state',  'modalidad', 'tipo', 'subtipo', 'date'])
+    victimas = victimas[columnOrder]
+    victimas.count = victimas['count'].str.replace(',', '')
+    #victimas.state = victimas["state"].map(titlecase)
+    victimas["fuero"] = "COMUN"
+    victimas['state_code'] = victimas.state_code.apply(int)
+    victimas['count'] = victimas['count'].apply(int)
+    assert(len(victimas['state'].unique()) == 32)
+    assert(len(victimas['modalidad'].unique()) == 1)
+    victimas['date'] = victimas['date'].str.slice(0, 7)
+    del victimas['year']
+    del victimas['month']
+    return victimas
+
+def clean_federal(file):
+    state_codes = pd.read_csv("data/state_codes.csv")
+    population = pd.read_csv("data/pop_states.csv")
+    df = pd.read_csv("victimas-csv/" +  file, skiprows=1, encoding = 'utf-8')
+    df = df.drop(df.columns[0], axis=1)
+
+    assert(all(df.columns == [u'V\xedctimas', u'Enero', u'Febrero', u'Marzo',
+                                 u'Abril', u'Mayo', u'Junio', u'Julio', u'Agosto',
+                                 u'Septiembre', u'Octubre', u'Noviembre', u'Diciembre',
+                                 u'Unnamed: 14']))
+    df=df.dropna(axis=1, how='all')
+    df=df.dropna(axis=0, how='all')
+    df = pd.melt(df, id_vars=u"V\xedctimas")
+    for k, v in mapping:
+        df['variable'] = df['variable'].replace(k, v)
+    df.columns = ['state', 'month', 'count']
+    df['date'] = '2015' + '-' + df['month'] + '-01'
+    df['year'] = '2015'
+    df = df[df['state'] != 'NACIONAL']
+    df = df[df['state'] != '']
+    df["modalidad"] = "PRIV. DE LA LIBERTAD (SECUESTRO)"
+    df['tipo'] = "SECUESTRO"
+    df['subtipo'] = "SECUESTRO"
+    df = pd.merge(df, state_codes, how = 'left')
+    df = pd.merge(df, population, how = 'left')
+    df.count = df['count'].str.replace(',', '')
+    #df.state = df["state"].map(titlecase)
+    df = df[columnOrder]
+    df['fuero'] = 'FEDERAL'
+    df['state_code'] = df.state_code.apply(int)
+    df['count'] = df['count'].apply(int)
+    df['date'] = df['date'].str.slice(0, 7)
+    del df['year']
+    del df['month']
+    return df
+
+
+    
+
+
