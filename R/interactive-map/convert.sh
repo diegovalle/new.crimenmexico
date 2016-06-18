6207226585b16bd70d1935ae5c9d832bf71ce2c9
Diego Valle-Jones <diegovalle@gmail.com>
2016-06-17 20:00:02 -0500

    check if file exists before deleting

--- R/interactive-map/convert.sh
+++ R/interactive-map/convert.sh
@@ -1,2 +1,6 @@
-rm municipios-centroids.json
+echo "Converting the municipio centroids to GeoJSON"
+if [ -f municipios-centroids.json ]
+then
+    rm municipios-centroids.json
+fi
 ogr2ogr -f "GeoJSON" municipios-centroids.json municipios-centroids.vrt
