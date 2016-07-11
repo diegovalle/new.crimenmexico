b1da6743b207e196073d533e5a1f1b17698e1cc0
diego <diegovalle@gmail.com>
2016-07-11 15:43:04 -0500

    modify last_data var
    October 16 is the last date for which data is available

--- R/run_all.R
+++ R/run_all.R
@@ -2,7 +2,7 @@ source(file.path("src", "packages.R"))
 source(file.path("src", "functions.R"))
 source(file.path("src", "charts.R"))
 source(file.path("src", "json.R"))
-last_inegi_date <- '2015-06-01'
+last_inegi_date <- '2015-10-01'
 source(file.path("src", "json_historic.R"))
 source(file.path("src", "muncharts.R"))
 source(file.path("src", "interactive.R"))
