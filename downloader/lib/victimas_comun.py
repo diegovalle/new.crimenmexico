c4acf598c74b33086205b146e8c97ca54cf7634b
diego <diegovalle@gmail.com>
2017-07-21 03:32:51 -0500

    replace column named ANO with AÑO [Skip CI]

--- downloader/lib/victimas_comun.py
+++ downloader/lib/victimas_comun.py
@@ -137,8 +137,9 @@ def clean_comun_xls(url):
     
     if 'TOTAL' in df.columns:
       del df['TOTAL']
+    df.columns = df.columns.str.replace(u'ANO',u'AÑO')
     #assert structure of file
-    assert(all(df.columns == [u'ANO', u'INEGI', u'ENTIDAD', u'DELITO', u'MODALIDAD',
+    assert(all(df.columns == [u'AÑO', u'INEGI', u'ENTIDAD', u'DELITO', u'MODALIDAD',
                               u'ENERO', u'FEBRERO', u'MARZO',
                               u'ABRIL', u'MAYO', u'JUNIO', u'JULIO', u'AGOSTO',
                               u'SEPTIEMBRE', u'OCTUBRE', u'NOVIEMBRE', u'DICIEMBRE']))
