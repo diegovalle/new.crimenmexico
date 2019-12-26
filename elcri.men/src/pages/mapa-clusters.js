fb227a3c76de26ef3fbf733182bc96df1332ce50
diego <git@diegovalle.net>
2019-12-25 19:50:57 -0600

    Use the netlify tileserver [Skip CI]

--- elcri.men/src/pages/mapa-clusters.js
+++ elcri.men/src/pages/mapa-clusters.js
@@ -225,13 +225,7 @@ class ClusterMap extends React.Component {
           {...this.state.viewport}
           onViewportChange={this._onViewportChange}
           onHover={this._onHover}
-          //mapStyle={"https://tile.thunderforest.com/thunderforest.transport-v1.json?apikey=b0ba23bd00dd483fa3e5e6c6c2afe6d2"}
-          //mapStyle="https://api.maptiler.com/maps/topo/style.json?key=1BrlyjfV5ry5lknBp5S6"
-          //mapStyle="mapbox://styles/mapbox/streets-v11"
-          //mapStyle="http://localhost:8000/elcrimen-json/style_bw.json"
           mapStyle={this.state.mapStyle}
-          //mapStyle="https://tileserver-902ec.firebaseapp.com/municipio-tiles/style_bw.json"
-          //mapStyle="https://tileserver-902ec.firebaseapp.com/inegi-municipios/style.json"
           mapboxApiAccessToken={process.env.GATSBY_MAPBOX_TOKEN}
           mapOptions={{
             customAttribution: '© MapTiler <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors</a>',
