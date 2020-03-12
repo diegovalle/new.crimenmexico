20bf6596af9765cfa14d155e6fd6358f48b6e217
diego <git@diegovalle.net>
2020-03-12 11:58:15 -0600

    Subset fonts to make them smaller [Skip CI]

---
+++ elcri.men/src/assets/fonts/optimize.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Basic Latin characters, hyphen and nbsp
+unicode="U+0-FF,U+202F,U+2014"
+# Don't do --desubroutinize cuz otf relies on it
+optimization="--verbose --no-hinting --drop-tables+=GSUB"
+# woff
+pyftsubset "source-sans-pro/source-sans-pro-v13-latin-regular.woff" --unicodes="$unicode" $optimization --flavor=woff --with-zopfli
+
+# woff2
+pyftsubset "source-sans-pro/source-sans-pro-v13-latin-regular.woff" --unicodes="$unicode" $optimization --flavor=woff2
+
+
+# woff
+pyftsubset "source-sans-pro/source-sans-pro-v13-latin-900.woff" --unicodes="$unicode" $optimization --flavor=woff --with-zopfli
+
+# woff2
+pyftsubset "source-sans-pro/source-sans-pro-v13-latin-900.woff" --unicodes="$unicode" $optimization --flavor=woff2
+
+# woff
+pyftsubset "roboto-condensed/roboto-condensed-v18-latin-300.woff" --unicodes="$unicode" $optimization --flavor=woff --with-zopfli
+
+# woff2
+pyftsubset "roboto-condensed/roboto-condensed-v18-latin-300.woff" --unicodes="$unicode" $optimization --flavor=woff2
+
+# woff
+pyftsubset "roboto-condensed/roboto-condensed-v18-latin-700.woff" --unicodes="$unicode" $optimization --flavor=woff --with-zopfli
+
+# woff2
+pyftsubset "roboto-condensed/roboto-condensed-v18-latin-700.woff" --unicodes="$unicode" $optimization --flavor=woff2
+
+# woff
+pyftsubset "roboto-condensed/roboto-condensed-v18-latin-regular.woff" --unicodes="$unicode" $optimization --flavor=woff --with-zopfli
+
+# woff2
+pyftsubset "roboto-condensed/roboto-condensed-v18-latin-regular.woff" --unicodes="$unicode" $optimization --flavor=woff2
