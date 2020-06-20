0880a20adcad62c29d24a64263dc5a90223c7378
diego <git@diegovalle.net>
2020-06-20 12:16:00 -0500

    Use gdown to download the crime data

--- download.sh
+++ download.sh
@@ -3,39 +3,34 @@
 set -euo pipefail #exit on error, undefined and prevent pipeline errors
 IFS=$'\n\t'
 
-COOKIE_TMP=$(mktemp)
-
 SNSP_DIR=clean/snsp-data
 
 ESTADOS_FC_ZIP=$SNSP_DIR/estados_fc_original.zip
 MUN_FC_ZIP=$SNSP_DIR/municpios_fc_original.zip
 ESTADOS_VIC_ZIP=$SNSP_DIR/estados_vic_original.zip
 
-drive_direct_download() {
-    ID="$(echo "$1" | sed "s|https://drive.google.com/open?id=||g")"
-    echo "https://drive.google.com/uc?export=download&id=$ID"
-}
-
 estatal_download() {
     regex="(?<=href=\")[^\"][^\"]*(?=\">$2 \d{4} - \d{4}<\/a><\/p>)"
     INTERMEDIATE_LINK=$(curl -s -L  "$1" | \
                                 grep -Po "$regex" | tail -n1 | \
-                                sed 's| |%20|g')
-    
-    drive_direct_download "$INTERMEDIATE_LINK"
+                                sed 's| |%20|g'  | grep -oP '(?<=/)[0-9a-zA-Z_-]{20,}(?=/)')
+    echo "$INTERMEDIATE_LINK"
+    gdown "https://drive.google.com/uc?id=$INTERMEDIATE_LINK" -O "$3"
+    #drive_direct_download "$INTERMEDIATE_LINK"
 }
 
 municipal_fc_download() {
-    #ggID='1FoFXpt4OeEXP8qeDzPKU-qky1f5iL8Gh'  
+    #ggID='1FoFXpt4OeEXP8qeDzPKU-qky1f5iL8Gh'
     ggID=$(curl -s  "$1" | \
                          grep -Po "(?<=href=\")[^\"][^\"]*(?=\">Municipal \d{4} - \d{4}<\/a>&nbsp;)" | \
-                         sed 's| |%20|g' | sed 's|https://drive.google.com/open?id=||g')
-    echo "ggId: $ggID" 
-    ggURL='https://drive.google.com/uc?export=download' 
-    curl -sc "$COOKIE_TMP" "${ggURL}&id=${ggID}" >/dev/null  
-    getcode="$(awk '/_warning_/ {print $NF}' $COOKIE_TMP)"  
-    echo "getcode: $getcode"
-    curl -Lb "$COOKIE_TMP" "${ggURL}&confirm=${getcode}&id=${ggID}" -o "$MUN_FC_ZIP"
+                         sed 's| |%20|g' | grep -oP '(?<=/)[0-9a-zA-Z_-]{20,}(?=/)')
+    echo "ggId: $ggID"
+    #ggURL='https://drive.google.com/uc?export=download'
+    #curl -sc "$COOKIE_TMP" "${ggURL}&id=${ggID}" >/dev/null
+    #getcode="$(awk '/_warning_/ {print $NF}' $COOKIE_TMP)"
+    #echo "getcode: $getcode"
+    #curl -Lb "$COOKIE_TMP" "${ggURL}&confirm=${getcode}&id=${ggID}" -o "$MUN_FC_ZIP"
+    gdown "https://drive.google.com/uc?id=$ggID" -O "$MUN_FC_ZIP"
 }
 
 convert_to_csv() {
@@ -55,13 +50,17 @@ convert_to_csv() {
     fi
 }
 
+if ! [ -x "$(command -v gdown)" ]; then
+    pip install gdown
+fi
 
 URL="https://www.gob.mx/sesnsp/acciones-y-programas/incidencia-delictiva-del-fuero-comun-nueva-metodologia?state=published"
-curl -L -s -o "$ESTADOS_FC_ZIP" "$(estatal_download "$URL" "Estatal")"
-#curl -s -Lb "$COOKIE_TMP" -o "$MUN_FC_ZIP" "$(municipal_fc_download "$URL")"
 municipal_fc_download "$URL"
+
+estatal_download "$URL" "Estatal" "$ESTADOS_FC_ZIP"
+
 URL_VIC="https://www.gob.mx/sesnsp/acciones-y-programas/victimas-nueva-metodologia?state=published"
-curl -s -L -o "$ESTADOS_VIC_ZIP" "$(estatal_download "$URL_VIC" "V&iacute;ctimas")"
+estatal_download "$URL_VIC" "V&iacute;ctimas" "$ESTADOS_VIC_ZIP"
 
 convert_to_csv "$ESTADOS_FC_ZIP" estados
 convert_to_csv "$MUN_FC_ZIP" municipios
