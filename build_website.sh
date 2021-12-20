ac576e144b37b48fd26d2f62e9a39fe2b26e60e8
diego <git@diegovalle.net>
2021-12-20 10:35:40 -0600

    Don't ssh

--- build_website.sh
+++ build_website.sh
@@ -45,10 +45,10 @@ fi
 cp R/interactive-map/municipios*.json elcri.men/static/elcrimen-json/
 
 # Copy the files to a backup server
-if [ "$CI" = true ] ; then
+#if [ "$CI" = true ] ; then
   # Upload a copy of the database
-  rsync --exclude='.git/' --exclude='nm*.csv.gz' --include='*.db' -az --compress-level=9 --stats -e 'ssh  -o StrictHostKeyChecking=no -i ~/.ssh/crimenmexico' --delete ~/new.crimenmexico  crimenmexico@"$IPADDRESS":/home/crimenmexico
-fi
+#  rsync --exclude='.git/' --exclude='nm*.csv.gz' --include='*.db' --include='*/static/smooth-latest.png' --include='*/R/graphs/*.png'-az --compress-level=9 --stats -e 'ssh  -o StrictHostKeyChecking=no -i ~/.ssh/crimenmexico' --delete ~/new.crimenmexico  crimenmexico@"$IPADDRESS":/home/crimenmexico
+#fi
 
 export GATSBY_TELEMETRY_DISABLED=1
 (cd elcri.men && npm install && gatsby build --verbose)
