9512f6ca7040ecb47d95e6b63aca5effd11894b3
diego <git@diegovalle.net>
2024-09-20 11:32:02 -0600

    Remove partytown and add GA4 plugin

--- elcri.men/src/components/layout.js
+++ elcri.men/src/components/layout.js
@@ -4,21 +4,10 @@ import { IntlProvider } from 'react-intl'
 import Footer from '../components/Footer'
 import { Script } from 'gatsby'
 
-const Layout = ({ locale, children, path, wide }) => {
+const Layout = (props) => {
+  const { locale, children, path, wide } = props
   return (
     <React.Fragment>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=G-SMLSV8EVFV`}
-        strategy="off-main-thread"
-      />
-      <Script id="gtag-config" strategy="off-main-thread" forward={[`gtag`]}>
-        {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments)};
-    gtag('js', new Date());
-    gtag('config', 'G-SMLSV8EVFV', { send_page_view: false })
-  `}
-      </Script>
       <div
         className="container is-fluid"
         style={
