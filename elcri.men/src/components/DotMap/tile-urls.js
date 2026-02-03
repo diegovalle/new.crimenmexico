5bc63ef7a162c7ebc064284c04e84923288c13d0
Diego Valle-Jones <git@diegovalle.net>
2026-02-03 11:38:56 -0600

    Remove tebi.io

--- elcri.men/src/components/DotMap/tile-urls.js
+++ elcri.men/src/components/DotMap/tile-urls.js
@@ -12,11 +12,6 @@ const osmTilesUrl =
 // const osmSpriteUrl = `https://tiles-kinsta.elcri.men/sprites/sprite`
 // const osmGlyphsUrl = `https://tiles-kinsta.elcri.men/font/{fontstack}/{range}.pbf`
 
-//const osmSpriteUrl = `https://tiles-tebi.elcri.men/sprites/sprite`
-//const osmGlyphsUrl = `https://tiles-tebi.elcri.men/font/{fontstack}/{range}.pbf`
-// const osmTilesUrl =
-//   'https://tiles-tebi.elcri.men/mexico-tiles/{z}/{x}/{y}.pbf.html'
-
 exports.osmSpriteUrl = osmSpriteUrl
 exports.osmGlyphsUrl = osmGlyphsUrl
 exports.osmTilesUrl = osmTilesUrl
