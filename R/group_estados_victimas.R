073d67d51554cc7461438def2ca78821eb46e517
Diego Valle-Jones <git@diegovalle.net>
2026-02-21 19:25:28 -0600

    feat: Add R script to recode victim age categories in 2026 data and update the download script to process it.

---
+++ R/group_estados_victimas.R
@@ -0,0 +1,29 @@
+library(dplyr)
+library()
+
+df <- read.csv("clean/snsp-data/estados_victimas2026.csv", fileEncoding = "windows-1252")
+unique(df$Rango.de.edad)
+df2_recoded <- 
+  mutate(df, 
+    Rango.de.edad = recode(Rango.de.edad,
+                 "0 a 12 años" = "Menores de edad (0-17)",
+                 `13 a 17 años` = "Menores de edad (0-17)",
+                 `18 a 29 años` = "Adultos (18 y más)",
+                 "30 a 60 años" = "Adultos (18 y más)",
+                 "Más de 60 años" = "Adultos (18 y más)",
+                 "No especificado" = 'No especificado'
+    )
+  )
+
+df_summarized <- aggregate(. ~ Año + Clave_Ent + Entidad + Bien.jurídico.afectado + 
+                             Tipo.de.delito + Subtipo.de.delito + Modalidad + 
+                             Sexo + Rango.de.edad, 
+                           data = df2_recoded, 
+                           FUN = sum, 
+                           na.rm = TRUE, 
+                           na.action = na.pass)
+
+write.csv(df_summarized, 
+          "clean/snsp-data/estados_victimas2026.csv", 
+          fileEncoding = "windows-1252",
+          row.names = FALSE)
