a9915a6514f6f19e16054adf8b053093d34d41c8
Diego Valle-Jones <git@diegovalle.net>
2026-04-29 11:05:33 -0600

    Update alternate tile urls

--- elcri.men/src/components/DotMap/tile-urls.js
+++ elcri.men/src/components/DotMap/tile-urls.js
@@ -1,16 +1,20 @@
+// const osmTilesUrl =
+//   'https://tiles-n.elcri.men/mexico-tiles/{z}/{x}/{y}.pbf'
+// no fonts or sprites on surge
+// const osmTilesUrl =
+//   'https://1777226290242-tiles-elcrimen.surge.sh/mexico-tiles/{z}/{x}/{y}.pbf'
+
 // const osmSpriteUrl = 'https://tiles-n.elcri.men/sprites/sprite'
 // const osmGlyphsUrl =
 //   'https://tiles-n.elcri.men/font/{fontstack}/{range}.pbf'
-// const osmTilesUrl =
-//   'https://tiles-n.elcri.men/mexico-tiles/{z}/{x}/{y}.pbf'
-
-const osmSpriteUrl = 'https://elcri.men/tiles/sprites/sprite'
-const osmGlyphsUrl = 'https://elcri.men/tiles/font/{fontstack}/{range}.pbf'
+// const osmSpriteUrl = `https://tiles-kinsta.elcri.men/sprites/sprite`
+// const osmGlyphsUrl = `https://tiles-kinsta.elcri.men/font/{fontstack}/{range}.pbf`
 
 const osmTilesUrl =
   'https://tiles-kinsta.elcri.men/mexico-tiles/{z}/{x}/{y}.pbf.html'
-// const osmSpriteUrl = `https://tiles-kinsta.elcri.men/sprites/sprite`
-// const osmGlyphsUrl = `https://tiles-kinsta.elcri.men/font/{fontstack}/{range}.pbf`
+
+const osmSpriteUrl = 'https://elcri.men/tiles/sprites/sprite'
+const osmGlyphsUrl = 'https://elcri.men/tiles/font/{fontstack}/{range}.pbf'
 
 exports.osmSpriteUrl = osmSpriteUrl
 exports.osmGlyphsUrl = osmGlyphsUrl
